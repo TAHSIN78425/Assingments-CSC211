0b60200ff3212a28589033647e323ee60b58e84c
TAHSIN78425 <125177892+TAHSIN78425@users.noreply.github.com>
2023-02-18 01:01:27 -0500

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        HeartRates heartRates=new HeartRates("TAHSIN","RAHMAN",2001,05,12);
-        heartRates.PersonsAge();
-        heartRates.HearRateCalc();
-
-    }
-}
