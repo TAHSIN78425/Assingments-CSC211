47581642c1bea482693125a57ca7485789bb584b
TAHSIN78425 <125177892+TAHSIN78425@users.noreply.github.com>
2023-02-18 00:38:21 -0500

    Update Main.java

--- Assingment 1 CSC_22100/src/Main.java
+++ Assingment 1 CSC_22100/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         HeartRates heartRates=new HeartRates("TAHSIN","RAHMAN",2001,05,12);
         System.out.println("Name of the patient is "+ heartRates.getFirstname()+" "+heartRates.getLastname());
-        System.out.println("Patient's Date of birth is "+ heartRates.getDay()+" "+ heartRates.getMonth()+" "+heartRates.getYear());
+       System.out.println("Patient's Date of birth is "+ + heartRates.getMonth()+"/"+heartRates.getDay()+"/"+heartRates.getYear());
         heartRates.PersonsAge();
         heartRates.MaximumHeartRate();
         heartRates.TargetHeartRate();
